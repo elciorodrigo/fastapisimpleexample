999aded24f59eb71e5930ed7f152cabd83f705e1
Elcio Silva <elcio.silva@5by5.com.br>
2023-01-06 14:04:07 -0300

    add async on request

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def home():
+
+@app.get("/{value}")
+async def home(value: str):
     return {
-        "message": "hello world"
+        "message": "value is %s" % value
     }
 
+
 if __name__ == "__main__":
-    uvicorn.run("app:app",host="127.0.0.1", port=8080)
+    uvicorn.run("app:app", host="127.0.0.1", port=8080)
